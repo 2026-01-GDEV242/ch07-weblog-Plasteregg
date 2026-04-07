f610ac044159a8c40670b74100f72feeab22e4b5
Plasteregg <Awright82000@gmail.com>
2026-04-06 21:49:01 -0400

    LogAnalyzer being able to take string

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -6,26 +6,21 @@
  */
 public class LogAnalyzer
 {
-    // Where to calculate the hourly access counts.
     private int[] hourCounts;
-    // Use a LogfileReader to access the data.
     private LogfileReader reader;
 
-    /**
-     * Create an object to analyze hourly web accesses.
-     */
     public LogAnalyzer()
     { 
-        // Create the array object to hold the hourly
-        // access counts.
         hourCounts = new int[24];
-        // Create the reader to obtain the data.
         reader = new LogfileReader();
     }
 
-    /**
-     * Analyze the hourly access data from the log file.
-     */
+    public LogAnalyzer(String filename)
+    {
+        hourCounts = new int[24];
+        reader = new LogfileReader(filename);
+    }
+
     public void analyzeHourlyData()
     {
         while(reader.hasNext()) {
@@ -35,11 +30,6 @@ public class LogAnalyzer
         }
     }
 
-    /**
-     * Print the hourly counts.
-     * These should have been set with a prior
-     * call to analyzeHourlyData.
-     */
     public void printHourlyCounts()
     {
         System.out.println("Hr: Count");
@@ -48,9 +38,6 @@ public class LogAnalyzer
         }
     }
     
-    /**
-     * Print the lines of data read by the LogfileReader
-     */
     public void printData()
     {
         reader.printData();
